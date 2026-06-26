46781e5493a7a02609e41a64f3a5cc027761e747
Akshat Jain <akshatjaincs18@gmail.com>
2026-06-27 04:08:12 +0530

    ArraysStringsBasics CF_71A_WayTooLongWords

---
+++ ArraysAndStrings/Basics/CF_71A_WayTooLongWords.java
@@ -0,0 +1,25 @@
+package ArraysAndStrings.Basics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CF_71A_WayTooLongWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<String> words = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            words.add(sc.next());
+
+        for (int i = 0; i < words.size(); i++) {
+            String word = words.get(i);
+            if(word.length() > 10){
+                System.out.println(word.charAt(0) + "" + (word.length() - 2) + "" + word.charAt(word.length() - 1));
+            }
+            else
+                System.out.println(word);
+        }
+        sc.close();
+    }
+}
